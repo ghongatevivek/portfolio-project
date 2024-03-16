86d44dbe7fb5e9d989ad6599ac24a4a5a1e56866
ghongatevivek <ghongatevivek1212@gmail.com>
2024-03-16 16:08:20 +0530

    Setup vue dependency & first vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import App from './components/New.vue'
+
+createApp(App).mount('#app');
---
+++ resources/js/components/New.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1> from vue </h1>
+</template>
